bd4e3b5c2864b6917beb99078e16436780a75bbd
Developerr18 <dineshnikum1@gmail.com>
2025-09-08 15:52:02 +0530

    build api endpoints for add and get food items

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,38 @@
+import FoodModel from "../models/foodModel.js";
+import fs from "fs";
+
+/////////////////////////////////////////////////////
+// Add Food Item
+const addFood = async (req, res) => {
+  const image_filename = `${req.file.filename}`;
+
+  const food = new FoodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food Added" });
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+////////////////////////////////////////////////
+// All Food List
+const listFoods = async (req, res) => {
+  try {
+    const foods = await FoodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { addFood, listFoods };
---
+++ backend/routes/foodRoute.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { addFood, listFoods } from "../controllers/foodController.js";
+import multer from "multer";
+
+const foodRouter = express.Router();
+
+///////////////////////////////////////
+// Image storage engine
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, callback) => {
+    return callback(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+/////////////////////////////////////////
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFoods);
+
+export default foodRouter;
