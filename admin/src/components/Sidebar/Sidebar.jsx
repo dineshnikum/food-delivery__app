01a91fc823a6844214e9aae032552a08fc8881d8
Developerr18 <dineshnikum1@gmail.com>
2025-09-13 15:56:25 +0530

    create sidebar UI

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,25 @@
 import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} />
+          <p>Add Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} />
+          <p>List Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} />
+          <p>Orders</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
