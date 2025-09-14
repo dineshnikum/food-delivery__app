fae91dc497b5657672becd0e3256a4f77ce42dd4
Developerr18 <dineshnikum1@gmail.com>
2025-09-14 10:09:46 +0530

    create register user controller to validate and create user

---
+++ backend/controllers/userController.js
@@ -0,0 +1,51 @@
+import UserModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+// Login user
+const loginUser = async (req, res) => {};
+
+// Register user
+const registerUser = async (req, res) => {
+  const { name, password, email } = req.body;
+  try {
+    const isExists = await UserModel.findOne({ email });
+    if (isExists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    // validating email format & strong password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new UserModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({ success: true, token });
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { loginUser, registerUser };
