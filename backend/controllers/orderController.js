0698b3a89b08666354981f2a78fd0d0eac19ea27
Developerr18 <dineshnikum1@gmail.com>
2025-09-15 18:43:55 +0530

    create order route

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,8 @@
+import OrderModel from "../models/orderModel.js";
+import UserModel from "../models/userModel.js";
+import stripe from "stripe";
+
+// Placing user order for frontend
+const placeOrder = async (req, res) => {};
+
+export { placeOrder };
