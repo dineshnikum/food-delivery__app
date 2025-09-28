00d73c8b4a7d584bf32a1632463f7a3360fad9e5
Developerr18 <dineshnikum1@gmail.com>
2025-09-28 16:14:56 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
-  const url = "https://food-delivery-app-cepk.onrender.com"
+  const url = "https://food-delivery-backend-vtr6.onrender.com"
 
   const fetchFoodList = async () => {
     const response = await axios.get(`${url}/api/food/list`);
